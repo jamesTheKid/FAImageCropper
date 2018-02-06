af7c5c50bef6031a6807778a1e4dc4f384493298
Jimmy KUMAKO <kumako.jimmy@gmail.com>
2018-02-06 03:50:18 +0000

    Add library selection.

--- FAImageCropper/Controllers/FAImageCropperVC.swift
+++ FAImageCropper/Controllers/FAImageCropperVC.swift
@@ -11,23 +11,34 @@ import Photos
 
 class FAImageCropperVC: UIViewController {
   
+  
     
     // MARK: IBOutlets
   
     @IBOutlet weak var globalScrollView: UIScrollView!
     @IBOutlet weak var selectedAlbumButton: UIButton!
+  
     @IBOutlet weak var arrowImageView: UIImageView!
+  
+    @IBOutlet weak var bottomToolsView: UIStackView!
+  
+    @IBOutlet weak var cancelButton: UIButton!
+    @IBOutlet weak var nextButton: UIButton!
   
     @IBOutlet weak var cameraButton: UIButton!
     @IBOutlet weak var librayButton: UIButton!
   
-    var didSetupConstraint = false
+  
   
     // MARK: Public Properties
   
+  
+    var didSetupConstraint = false
+    var albumName: String!
+  
     let cameraView: FACameraView = FACameraView.instance()
     let libraryView: FALibraryView = FALibraryView.instance()
-  
+    let albumView = FAPhotoPickerAlbumView.instance()
     
     // MARK: LifeCycle
     
@@ -104,6 +115,8 @@ class FAImageCropperVC: UIViewController {
     }
     
     private func viewConfigurations() {
+      
+      albumView.delegate = self
     
       globalScrollView.contentSize = CGSize(width: self.view.frame.size.width*2, height: self.globalScrollView.frame.height)
     
@@ -129,24 +142,36 @@ class FAImageCropperVC: UIViewController {
       
       //navigationBarConfigurations()
       
+      albumName = "All Photos"
+      selectedAlbumButton.setTitle(albumName, for: .normal)
+      let contraint = selectedAlbumButton.titleLabel?.intrinsicContentSize
+      selectedAlbumButton.updateConstraint(attribute: .width, value: (contraint?.width)!)
+      
+      
+      let downIcon = UIImage(named: "arrowDown.png", in: Bundle(for: self.classForCoder), compatibleWith: nil)?.withRenderingMode(.alwaysTemplate)
+      
+      arrowImageView.image = downIcon
+      albumView.frame = CGRect(x: 0, y: view.frame.height, width: view.frame.width, height: view.frame.height - 44.0)
+      
+      view.addSubview(self.albumView)
+      
     }
   
-  
-  
-  override func viewDidLayoutSubviews() {
-    
-    
-    if didSetupConstraint == false {
-      self.libraryView.frame = CGRect(origin:CGPoint.zero, size: self.globalScrollView.frame.size)
-      let cameraViewFrameOrigin = CGPoint(x: self.view.frame.size.width,y: 0)
-      self.cameraView.frame = CGRect(origin:cameraViewFrameOrigin , size: self.globalScrollView.frame.size)
-      self.cameraView.startSession()
-      didSetupConstraint = true
+    override func viewDidLayoutSubviews() {
+      
+      if didSetupConstraint == false {
+        self.libraryView.frame = CGRect(origin:CGPoint.zero, size: self.globalScrollView.frame.size)
+        let cameraViewFrameOrigin = CGPoint(x: self.view.frame.size.width,y: 0)
+        self.cameraView.frame = CGRect(origin:cameraViewFrameOrigin , size: self.globalScrollView.frame.size)
+        albumView.frame = CGRect(x: 0, y: view.frame.height, width: view.frame.width, height: view.frame.height - 44.0)
+        
+        self.cameraView.startSession()
+        didSetupConstraint = true
+      }
+      
+      
     }
-    
-    
-  }
-    
+  
     private func navigationBarConfigurations() {
     
         navigationController?.navigationBar.backgroundColor = .black
@@ -166,8 +191,8 @@ class FAImageCropperVC: UIViewController {
     // MARK: IBActions
     @IBAction func librayAction(_ sender: UIButton) {
       self.arrowImageView.isHidden = false
-      //self.nextButton.isHidden = false
-      //selectAlbumButton.setTitle(albumName, for: .normal)
+      self.nextButton.isHidden = false
+      selectedAlbumButton.setTitle(albumName, for: .normal)
       globalScrollView.setContentOffset(CGPoint(x:0, y:0), animated: true)
       librayButton.setTitleColor(UIColor.white, for: .normal)
       cameraButton.setTitleColor(UIColor.lightGray, for: .normal)
@@ -175,17 +200,62 @@ class FAImageCropperVC: UIViewController {
   
     @IBAction func cameraAction(_ sender: UIButton) {
       self.arrowImageView.isHidden = true
-      //self.nextButton.isHidden = true
-      //selectAlbumButton.setTitle("Photo", for: .normal)
+      self.nextButton.isHidden = true
+      selectedAlbumButton.setTitle("Photo", for: .normal)
       globalScrollView.setContentOffset(CGPoint(x:self.globalScrollView.frame.width, y:0), animated: true)
       cameraButton.setTitleColor(UIColor.white, for: .normal)
       librayButton.setTitleColor(UIColor.lightGray, for: .normal)
+    }
+  
+  
+    @IBAction func showAlbums(_ sender: UIButton) {
+      
+       if !selectedAlbumButton.isSelected {
+        selectedAlbumButton.isSelected = true
+        
+        
+        UIView.animate(withDuration: 0.3, animations: {
+          var rect = self.bottomToolsView.frame
+          rect.origin.y = self.view.frame.height
+          self.bottomToolsView.frame = rect
+        })
+        
+        UIView.animate(withDuration: 1, delay: 0.3, usingSpringWithDamping: 12, initialSpringVelocity: 12, options: .layoutSubviews, animations: {
+          self.albumView.frame = CGRect(x: 0, y: 44, width: self.view.frame.width, height: self.view.frame.height - 44.0)
+          self.arrowImageView.transform = CGAffineTransform(rotationAngle: CGFloat(Double.pi))
+          
+          self.cancelButton.isHidden = true
+          self.nextButton.isHidden = true
+        }) { (isComplete) in
+          
+          
+        }
+      }else {
+        // handle hidden album list.
+        UIView.animate(withDuration: 0.3, animations: {
+          self.albumView.frame = CGRect(x: 0, y: self.view.frame.height, width: self.view.frame.width, height: self.view.frame.height - 44.0)
+          self.arrowImageView.transform = CGAffineTransform(rotationAngle: CGFloat(Double.pi * 2))
+          self.cancelButton.isHidden = false
+          self.nextButton.isHidden = false
+        })
+        
+        selectedAlbumButton.isSelected = false
+        UIView.animate(withDuration: 1, delay: 0.3, usingSpringWithDamping: 12, initialSpringVelocity: 12, options: .layoutSubviews, animations: {
+          
+          var rect = self.bottomToolsView.frame
+          rect.origin.y = self.view.frame.height - 44.0
+          self.bottomToolsView.frame = rect
+        }, completion: { (isComplete) in
+          
+        })
+        
+      }
     }
 
 }
 
 
-extension FAImageCropperVC:  FACameraViewDelegate{
+extension FAImageCropperVC:  FACameraViewDelegate, FAPhotoPickerAlbumViewDelegate{
   
   func didShootPhoto(image: UIImage, metaData: [String : Any]) {
     
@@ -194,7 +264,47 @@ extension FAImageCropperVC:  FACameraViewDelegate{
     //performSegue(withIdentifier: "FADetailViewSegue", sender: nil)
   }
   
+  
+  func didSeletctAlbum(album: AlbumModel) {
+    
+    albumName = album.name
+    selectedAlbumButton.setTitle(albumName, for: .normal)
+    
+    let contentSize = selectedAlbumButton.titleLabel?.intrinsicContentSize
+    
+    selectedAlbumButton.updateConstraint(attribute: .width, value: (contentSize?.width)!)
+    libraryView.albumSelected(fetchResult: album.assets)
+    /*
+    library.images = album.assets
+    library.collectionView.reloadData()
+    library.collectionView.selectItem(at: IndexPath.init(row: 0, section: 0), animated: false, scrollPosition: .bottom)
+    album.fetchFirstImage { (image) in
+      self.library.setupFirstLoadingImageAttribute(image: image)
+      
+    }
+    */
+    
+    UIView.animate(withDuration: 0.3, animations: {
+      self.albumView.frame = CGRect(x: 0, y: self.view.frame.height, width: self.view.frame.width, height: self.view.frame.height - 44.0)
+      self.arrowImageView.transform = CGAffineTransform(rotationAngle: CGFloat(Double.pi * 2))
+      self.cancelButton.isHidden = false
+      self.nextButton.isHidden = false
+    })
+    
+    selectedAlbumButton.isSelected = false
+    UIView.animate(withDuration: 1, delay: 0.3, usingSpringWithDamping: 12, initialSpringVelocity: 12, options: .layoutSubviews, animations: {
+      
+      var rect = self.bottomToolsView.frame
+      rect.origin.y = self.view.frame.height - 44.0
+      self.bottomToolsView.frame = rect
+    }, completion: { (isComplete) in
+      
+    })
+    
+  }
 }
+
+
 
 
 
